dc124972a6a17c887f7f4df4d9e088e612f715b8
Filip Sufitchi <fsufitchi@gmail.com>
2017-07-03 23:41:29 -0400

    Add interrupt signal processing

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
 
 	"github.com/fsufitch/prez-tweet/prez-tweet-server"
 )
 
 func main() {
-	log.Fatal(server.StartServer())
+	interruptChan := make(chan os.Signal)
+	signal.Notify(interruptChan, os.Interrupt)
+
+	serverQuitChan := make(chan error)
+	go func() {
+		serverQuitChan <- server.StartServer()
+	}()
+
+	select {
+	case <-interruptChan:
+		log.Println("Received interrupt. Quitting.")
+		break
+	case err := <-serverQuitChan:
+		log.Fatal(err)
+	}
 }
